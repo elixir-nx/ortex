1939e09658f33c43f3f3def2322eb35997faa7e8
Travis Morton <tmorton@relaypro.com>
2023-05-22 13:56:48 -0400

    v0.1.3

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Ortex.MixProject do
   def project do
     [
       app: :ortex,
-      version: "0.1.2",
+      version: "0.1.3",
       elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
